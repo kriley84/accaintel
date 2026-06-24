01fcfb93f9ea2a1eed92bd136f0ed7d33e8142ac
kriley84 <kentriley84@yahoo.com>
2026-06-24 01:31:30 +0100

    Refactor API handler to use https.get instead of fetch

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,23 +1,25 @@
+const https = require('https');
+
 exports.handler = async (event) => {
   const path = event.queryStringParameters?.path || '';
-  const url = 'https://v3.football.api-sports.io' + path;
-  try {
-    const response = await fetch(url, {
-      headers: { 'x-apisports-key': 'd65d0f5beaad8c0674a6392e3085cf60' }
+  const options = {
+    hostname: 'v3.football.api-sports.io',
+    path: path,
+    headers: { 'x-apisports-key': 'd65d0f5beaad8c0674a6392e3085cf60' }
+  };
+  return new Promise((resolve) => {
+    https.get(options, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        resolve({
+          statusCode: 200,
+          headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+          body: data
+        });
+      });
+    }).on('error', (e) => {
+      resolve({ statusCode: 500, body: JSON.stringify({ error: e.message }) });
     });
-    const data = await response.json();
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    };
-  } catch(e) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: e.message })
-    };
-  }
+  });
 };
